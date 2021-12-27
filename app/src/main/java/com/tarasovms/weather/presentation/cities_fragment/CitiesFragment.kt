40a2da3bedc211b848fec2d0057fa69f6c38e51c
Cruellok <80469992+Cruellok@users.noreply.github.com>
2021-12-27 20:50:38 +0400

    Initial commit

--- app/src/main/java/com/tarasovms/weather/presentation/cities_fragment/CitiesFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.tarasovms.weather.presentation.cities_fragment
-
-import androidx.fragment.app.Fragment
-
-class CitiesFragment: Fragment() {
-
-}
--- app/src/main/java/com/tarasovms/weather/presentation/map_fragment/MapFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.tarasovms.weather.presentation.map_fragment
-
-import androidx.fragment.app.Fragment
-
-class MapFragment: Fragment() {
-
-}
